72e59d140149035d34860d3bcdb76f3c4aee3bac
Petr Cervenka <petr@nanosolutions.io>
2020-08-13 15:43:44 +0800

    Fix it up

--- resources/js/components/Archive.vue
+++ resources/js/components/Archive.vue
@@ -14,85 +14,89 @@
           >
             <a
               :href="'/archive/'+day.format"
+              class="h3 nav-link"
               @click.prevent="loadDay(day.format)"
             >
               {{ day.calendar }}
             </a>
-          </li>
-          <ul />
-        </ul>
-      </div>
-    </div>
 
-    <div class="card w-100 mt-2">
-      <div class="card-header text-center text-secondary">
-        <strong>Show for {{ today }}</strong>
-      </div>
-      <div class="card-body">
-        <ul class="list-group list-group-striped list-group-flush">
-          <li
-            class="list-group-item p-1 rounded"
-            :class="{'list-group-item-success': slotPlaying === time}"
-            v-for="(show, time) in shows"
-            :key="time"
-          >
-            <button
-              class="btn"
-              @click="playArchive(time)"
-              :class="{'btn-success':slotPlaying == time,'btn-warning':slotPlaying !== time}"
+            <ul
+              class="list-group  list-group-flush"
+              v-if="day.format === today"
             >
-              <svg
-                v-if="slotPlaying !== time"
-                width="2em"
-                height="2em"
-                viewBox="0 0 16 16"
-                class="bi bi-stop"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
+              <li
+                class="list-group-item p-1 rounded"
+                :class="{'list-group-item-success': slotPlaying === time , 'list-group-item-danger': show.now, 'list-group-item-dark': isFuture(time) }"
+                v-for="(show, time) in shows"
+                :key="time"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"
-                />
+                <button
+                  class="btn"
+                  :disabled="isFuture(time)"
+                  @click="playArchive(time)"
+                  :class="{'btn-success':slotPlaying == time,'btn-warning':slotPlaying !== time}"
+                >
+                  <svg
+                    v-if="slotPlaying !== time"
+                    width="2em"
+                    height="2em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-stop"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"
+                    />
 
-              </svg>
+                  </svg>
 
-              <svg
-                v-if="slotPlaying === time"
-                width="2em"
-                height="2em"
-                viewBox="0 0 16 16"
-                class="bi bi-play"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z"
-              />
+                  <svg
+                    v-if="slotPlaying === time"
+                    width="2em"
+                    height="2em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-play"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  ><path
+                    fill-rule="evenodd"
+                    d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z"
+                  />
 
-              </svg>
-            </button>
-            <strong> {{ time }} </strong>
-            <strong class="float-right">
-              {{ toYourTime(time) }}
-              {{ isFuture(time) }}
-            </strong>
+                  </svg>
+                </button>
+                <strong> {{ time }} </strong>
+                <strong class="float-right">
+                  {{ toYourTime(time) }}
+                </strong>
 
-            <span
-              v-for="(person, pindex) in show.people"
-              :key="pindex"
-            >
-              <a
-                :href="person.link"
-                target="_blank"
-              >{{ person.name }}</a>
-            </span>
+                <span
+                  v-for="(person, pindex) in show.people"
+                  :key="pindex"
+                >
+                  <a
+                    :href="person.link"
+                    target="_blank"
+                  >{{ person.name }}</a>
+                </span>
 
-            <span class="text-muted float-right">
-              {{ show.desc }}
-            </span>
+                <span
+                  v-if="show.now"
+                  class="badge badge-info"
+                >
+                  <span v-if="isToday">Live now</span>
+                  <span v-else>TimeMachine now</span>
+
+                </span>
+
+                <span class="text-muted float-right">
+                  {{ show.desc }}
+                </span>
+              </li>
+            </ul>
           </li>
-          <ul />
         </ul>
       </div>
     </div>
@@ -127,6 +131,9 @@ export default {
   computed: {
     radioNow () {
       return moment().tz(this.$parent.radioTZ).format('HH:mm:ss')
+    },
+    isToday () {
+      return moment().format('YYYY-MM-DD') === this.today
     }
   },
   methods: {
@@ -135,10 +142,12 @@ export default {
       const now = moment().tz(this.$parent.radioTZ)
       console.log('show time ', playing)
       console.log('now time', now)
-      return now.diff(playing)
+      return now.diff(playing) < 0
     },
+
     toYourTime (time) {
-      return moment(this.today + ' ' + time, 'YYYY-MM-DD HH:mm:ss').tz(this.$parent.radioTZ).calendar()
+      const prague = moment(this.today + ' ' + time, 'YYYY-MM-DD HH:mm:ss').tz(this.$parent.radioTZ)
+      return prague.clone().tz(this.$parent.youTZ)
     },
     loadDay (day) {
       this.$parent.loaded = false
