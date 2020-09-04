4d1f2e6dad2348254a989f771019256827b52718
Petr Cervenka <petr@nanosolutions.io>
2020-09-04 14:22:05 +0800

    Add bugsnag

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
